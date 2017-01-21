fcc843960389c6cf07bfeb17155a5597086fe5bc
stas <kstas.85@gmail.com>
2017-01-21 02:00:26 +0200

    added button delete/submit

--- dynamic-table-js/data.js
+++ dynamic-table-js/data.js
@@ -1,7 +1,8 @@
 var tableData='{"table":[{"FirstName":"Stas","LastName":"Kaufman","Age":31},' +
     ' {"FirstName":"Roy","LastName":"Franco","Age":30},' +
     '{"FirstName":"Noa","LastName":"Gonda","Age":28},' +
-    '{"FirstName":"Bon","LastName":"Doggy","Age":4}]}';
+    '{"FirstName":"Bon","LastName":"Doggy","Age":4},' +
+    '{"FirstName":"Relin","LastName":"W","Age":24}]}';
 
 
 
--- dynamic-table-js/script.js
+++ dynamic-table-js/script.js
@@ -1,40 +1,110 @@
-
-document.addEventListener("DOMContentLoaded",
 function createBasicTable() {
+
     var objData = JSON.parse(tableData);
-    var table = document.createElement("TABLE");
-    document.getElementsByClassName('container')[0].appendChild(table);
-    table.setAttribute("class", "table table-bordered table-striped margin");
+    var holdElement;
+
+    holdElement = createAppendSet("div", document.getElementsByClassName('container')[0], "row");
+    holdElement = createAppendSet("div", holdElement, "col-md-12");
+    holdElement = createAppendSet("table", holdElement, "table table-bordered table-striped margin");
+    holdElement = createAppendSet("tbody", holdElement, null);
 
     var array = objData.table;
 
     var i;
     for (i = 0; i < array.length; i++) {
 
-        if(i == 0) {
-            var row = document.createElement("TR");
-            table.appendChild(row);
+        if (i == 0) {
+            holdElement = createAppendSet("tr", holdElement, null);
+
+            holdElement = createAppendSet("th", holdElement, null);
+            var text = document.createTextNode("Functionally");
+            holdElement.appendChild(text);
+
             var currElement = array[0];
-            var y;
-            for (y in currElement) {
+            var headerData;
+            for (headerData in currElement) {
+
                 var header = document.createElement("TH");
-                row.appendChild(header);
-                var text = document.createTextNode(y);
+                document.getElementsByTagName("tr")[0].appendChild(header);
+                text = document.createTextNode(headerData);
                 header.appendChild(text);
             }
         }
-        var row = document.createElement("TR");
-        table.appendChild(row);
+
+        holdElement = createAppendSet("tr", document.getElementsByTagName("tbody")[0], null);
+        holdElement = createAppendSet("td", holdElement, null);
+        holdElement = createAppendSet("span", holdElement, null);
+        holdElement = createAppendSet("button", holdElement, "btn btn-warning btn-sm")
+
+        text = document.createTextNode("Delete");
+        holdElement.appendChild(text);
+        holdElement.addEventListener("click", deleteButtons);
+
+        //
+        var arrayData = ["Fname:", "Lname:", "Age:"];
+        var j;
+        for (j = 0; j < arrayData.length; j++) {
+            holdElement = createAppendSet("span", document.getElementsByTagName("td")[i * 4], null);
+            text = document.createTextNode(arrayData[j]);
+            holdElement.appendChild(text);
+
+
+            var curHoladElement = createAppendSet("input", holdElement, "form-control")
+            curHoladElement.setAttribute("type", "text");
+
+        }
+
+        holdElement = createAppendSet("span", document.getElementsByTagName("td")[i * 4], null);
+        holdElement = createAppendSet("button", holdElement, "btn-success btn-sm");
+        holdElement.addEventListener("click", submitButton);
+        text = document.createTextNode("Submit");
+        holdElement.appendChild(text);
         var x;
         var currArray = array[i];
         for (x in currArray) {
-            var data = document.createElement("TD");
-            row.appendChild(data);
-            var text = document.createTextNode(currArray[x]);
-            data.appendChild(text);
+            holdElement = createAppendSet("td", document.getElementsByTagName("tr")[i + 1], null);
+            text = document.createTextNode(currArray[x]);
+            holdElement.appendChild(text);
         }
     }
-})
+}
+
+function createAppendSet(create, append, set) {
+    var createdElement = document.createElement(create);
+    append.appendChild(createdElement);
+    if (set != null) {
+        createdElement.setAttribute("class", set);
+    }
+    return createdElement;
+}
+
+function submitButton() {
+    var array = [];
+    //get to span element.
+    var input = this.parentNode.previousSibling;
+    var i = 0;
+    while (i < 3) {
+        array.push(input.childNodes[1].value);
+        input = input.previousSibling;
+        i++;
+    }
+
+    var output = this.parentNode.parentNode.nextSibling;
+    var i = 2;
+    while (i >= 0) {
+        output.innerHTML = array[i];
+        output = output.nextSibling;
+        i--;
+    }
+}
+
+function deleteButtons() {
+    var tdElement = this.parentNode.parentNode.nextSibling;
+    while (tdElement != null) {
+        tdElement.innerHTML = "";
+        tdElement = tdElement.nextSibling;
+    }
+}
 
 
 
